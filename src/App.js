6ac8223a01ed58eaebc62cbedda353a328748fdf
strawhatrag <sreeragbharathan791@gmail.com>
2023-12-28 15:31:25 +0530

    search filter added

--- src/App.js
+++ src/App.js
@@ -1,31 +1,40 @@
 import './App.css';
-import { Component } from 'react';
-
-
+import React, { Component } from 'react';
 
 class App extends Component {
-
-  constructor(){
+  constructor() {
     super();
-
     this.state = {
-     monsters:[]
-
-    }
+      monsters: [],
+      searchField: ''
+    };
   }
 
   componentDidMount() {
     fetch('https://jsonplaceholder.typicode.com/users')
       .then(response => response.json())
-      .then(users => this.setState({ monsters: users }, () => {console.log(this.state)}));
+      .then(users => this.setState({ monsters: users }));
   }
 
+  handleSearchChange = (event) => {
+    this.setState({ searchField: event.target.value });
+  }
 
   render() {
+    const { monsters, searchField } = this.state;
+    const filteredMonsters = monsters.filter(monster =>
+      monster.name.toLowerCase().includes(searchField.toLowerCase())
+    );
+
     return (
       <div className="App">
-        <input className='search-box' type='search' placeholder='search monster' onChange={() => {}}/>
-        {this.state.monsters.map((monster) => (
+        <input
+          className='search-box'
+          type='search'
+          placeholder='search monster'
+          onChange={this.handleSearchChange}
+        />
+        {filteredMonsters.map(monster => (
           <div key={monster.id}>
             <h1>{monster.name}</h1>
           </div>
